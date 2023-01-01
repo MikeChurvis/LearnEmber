ec4286219bdda63486a5bfadd00496da4f048fb4
Mike Churvis <mikechurvis@gmail.com>
2022-12-31 19:40:58 -0500

    Linted files
    CI should no longer complain. At least the lint job. Hopefully.

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,18 +1,15 @@
 import Controller from '@ember/controller';
 
 export default class CartController extends Controller {
-    get subtotal() {
-        return this.model.reduce(
-            (priceAcc, { price }) => priceAcc + price,
-            0
-        );
-    }
+  get subtotal() {
+    return this.model.reduce((priceAcc, { price }) => priceAcc + price, 0);
+  }
 
-    get tax() {
-        return 0.09 * this.subtotal;
-    }
+  get tax() {
+    return 0.09 * this.subtotal;
+  }
 
-    get total() {
-        return this.subtotal + this.tax;
-    }
+  get total() {
+    return this.subtotal + this.tax;
+  }
 }
